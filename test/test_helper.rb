7e3e48c5fd73e2eb3edac3dc3dea34bd26e46796
Robin van de Griend <robinvdgriend@outlook.com>
2016-01-05 13:58:50 +0100

    Add tests for user index pagination

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,6 +4,9 @@ require 'rails/test_help'
 require 'minitest/reporters'
 Minitest::Reporters.use!
 
+# Keep test schema up to date
+ActiveRecord::Migration.maintain_test_schema!
+
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
